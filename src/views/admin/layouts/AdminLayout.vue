89a84bb5d3adc3649ed53abfbda0a4c13cf1df1c
mahjouba <mano500mano13@gmail.com>
2024-04-22 00:26:05 +0200

    Signed-off-by: mahjouba <mano500mano13@gmail.com>

--- src/views/admin/layouts/AdminLayout.vue
+++ src/views/admin/layouts/AdminLayout.vue
@@ -7,7 +7,7 @@
 
 
       <div class="flex-1">
-        <Header />
+        <NavBar />
         <div class="p-8 text-cyan-700 bg-gray-200 h-full font-extrabold">This is The Content Section</div>
       </div>
   </div>
@@ -17,10 +17,11 @@
 
 import SideBar from "@/components/admin/SideBar.vue";
 import Header from "@/components/admin/NavBar.vue";
+import NavBar from "@/components/admin/NavBar.vue";
 
 export default {
   name: 'AdminLayout',
-  components: {Header, SideBar},
+  components: {NavBar , SideBar},
 };
 </script>
 
