ccb4680c259c1490783d42159e946e3c37babb16
mahjouba bouchouf <45915936+manomano500@users.noreply.github.com>
2024-08-03 23:35:14 +0200

    n

--- src/views/dashboard/layouts/AuthLayout.vue
+++ src/views/dashboard/layouts/AuthLayout.vue
@@ -5,7 +5,7 @@ export default {
 </script>
 
 <template>
-  <div class="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
+  <div class="flex  flex-1 flex-col justify-center ">
 
     <router-view></router-view>
 
